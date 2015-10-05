0ad72592063bf4b757469ad066958cc927897975
alexfieg <a_fieg02@uni-muenster.de>
2015-10-05 15:54:12 +0200

    Change S2 identification for gaseous only ( just quick and dirty )

---
+++ pax/plugins/peak_processing/ClassifyPeaks_gastpcms.py
@@ -0,0 +1,31 @@
+from pax import plugin, units
+
+
+class AdHocClassification(plugin.TransformPlugin):
+
+    def transform_event(self, event):
+
+        for peak in event.peaks:
+            # Don't work on noise and lone_hit
+            if peak.type in ('noise', 'lone_hit'):
+                continue
+
+            width = peak.range_area_decile[5]
+
+            if peak.area > 1:
+                # We don't have to worry about single electrons anymore
+                if width < 250 * units.ns:
+                    peak.type = 's1'
+                elif width > 350 * units.ns:
+                    peak.type = 's2'
+            else:
+                # Worry about SE-S1 identification.
+                if width < 75 * units.ns:
+                    peak.type = 's1'
+                else:
+                    if peak.area < 5:
+                        peak.type = 'coincidence'
+                    elif width > 100 * units.ns:
+                        peak.type = 's2'
+
+        return event
