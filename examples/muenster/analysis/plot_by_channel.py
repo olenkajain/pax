5c826dae71efc7329217e8c8ba8d234aab6db767
Axel Buß <a_buss09@uni-muenster.de>
2015-10-06 15:50:53 +0200

    moved ClassifyPeaks_gastpcms.py to muenster folder

--- examples/muenster/analysis/plot_by_channel.py
+++ examples/muenster/analysis/plot_by_channel.py
@@ -8,31 +8,25 @@ def make_plot(hits_df, field, xaxislabel = "x-axis", yaxislabel = "y-axis", nof_
     else:
         fig, ax = plt.subplots(nrows=7, ncols=2, sharex=True, sharey=True, squeeze=False, figsize=(20,14))
     plt.tight_layout()
-
-    ax[6][0].set_xlabel(xaxislabel)
-    ax[6][1].set_xlabel(xaxislabel)
-    for xi in range(7):
-            ax[xi][0].set_xlim(xlim)
+    for i, axi in enumerate(ax.reshape(-1)):
+        #ax[6][0].set_xlabel(xaxislabel)
+        #ax[6][1].set_xlabel(xaxislabel)
+            axi.set_xlim(xlim)
             if ylim:
-                ax[xi][0].set_ylim(ylim)
-            ax[xi][0].set_ylabel(yaxislabel)
-            ax[xi][0].set_yscale("log")
-            ax[xi][0].hist([xhits[field] for xhits in hits_df if (xhits["channel"] == xi)],
+                axi.set_ylim(ylim)
+            axi.set_ylabel(yaxislabel)
+            axi.set_yscale("log")
+            try:
+                axi.hist([xhits[field] for xhits in hits_df if (xhits["channel"] == i)],
                                 #range=(0,10),
                                 bins=nof_bins,
                                 histtype="step",
                                 label=legendlabel,
                                 **kwargs)
-            ax[xi][0].text(8, 1000, "Channel # %i"%xi)
-    for xi in range(7, 14):
-            ax[xi-7][1].set_yscale("log")
-            #ax[xi][yi].set_ylim(0,10000)
-            ax[xi-7][1].hist([xhits[field] for xhits in hits_df if (xhits["channel"] == xi)],
-                                #range=(0,10),
-                                bins=nof_bins,
-                                histtype="step",
-                                label=legendlabel,
-                                **kwargs)
-            ax[0][1].legend(framealpha=0.5)
-            ax[xi-7][1].text(8, 1000, "Channel # %i"%xi)
+            except ValueError:
+                axi.annotate("no hits", xy=(0.5,0.5), xycoords="axes fraction", horizontalalignment="center")
+
+            axi.annotate("Channel # %i"%i, xy=(0.8, 0.8), xycoords="axes fraction")
+
     #return fig
+
